c12652dcd75288d111b1d6bf96bbc7d35e8d1605
Yorick de Wid <yorickdewid@users.noreply.github.com>
2024-02-02 11:06:19 +0100

    Refactor protocol.rs to improve code readability

--- src/protocol.rs
+++ src/protocol.rs
@@ -2,12 +2,12 @@ use crate::{Frame, FrameBuilder, IdBuilder, Name, PGN};
 
 /// Create PGN request frame.
 pub fn request(da: u8, pgn: PGN) -> Frame {
-    let byte_array = pgn.to_le_bytes();
+    let pgn_bytes = pgn.to_le_bytes();
 
     let id = IdBuilder::from_pgn(PGN::Request).da(da).build();
 
     FrameBuilder::new(id)
-        .copy_from_slice(&[byte_array[0], byte_array[1], byte_array[2]])
+        .copy_from_slice(&[pgn_bytes[0], pgn_bytes[1], pgn_bytes[2]])
         .build()
 }
 
@@ -20,5 +20,28 @@ pub fn address_claimed(sa: u8, name: Name) -> Frame {
 
     FrameBuilder::new(id)
         .copy_from_slice(&name.to_bytes()[..])
+        .build()
+}
+
+/// Create acknowledgment frame.
+pub fn acknowledgement(sa: u8, pgn: PGN) -> Frame {
+    let pgn_bytes = pgn.to_le_bytes();
+
+    let id = IdBuilder::from_pgn(PGN::AcknowledgmentMessage)
+        .da(0xff)
+        .sa(sa)
+        .build();
+
+    FrameBuilder::new(id)
+        .copy_from_slice(&[
+            0x01,
+            0xff,
+            0xff,
+            0xff,
+            0xff,
+            pgn_bytes[0],
+            pgn_bytes[1],
+            pgn_bytes[2],
+        ])
         .build()
 }

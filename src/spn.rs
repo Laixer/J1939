d7ffc4b2dc1be4be4801bcf308265fc130e425ae
Yorick de Wid <yorickdewid@users.noreply.github.com>
2024-03-12 23:07:42 +0100

    Refactor code to improve performance and readability

--- src/spn.rs
+++ src/spn.rs
@@ -86,21 +86,22 @@ impl EngineTorqueMode {
         }
     }
 
-    pub fn to_value(mode: Self) -> u8 {
+    pub fn to_value(mode: Option<Self>) -> u8 {
         match mode {
-            Self::NoRequest => 0b0000,
-            Self::AcceleratorPedal => 0b0001,
-            Self::CruiseControl => 0b0010,
-            Self::PTOGovernor => 0b0011,
-            Self::RoadSpeedGovernor => 0b0100,
-            Self::ASRControl => 0b0101,
-            Self::TransmissionControl => 0b0110,
-            Self::ABSControl => 0b0111,
-            Self::TorqueLimiting => 0b1000,
-            Self::HighSpeedGovernor => 0b1001,
-            Self::BrakingSystem => 0b1010,
-            Self::RemoteAccelerator => 0b1011,
-            Self::Other => 0b1111,
+            Some(Self::NoRequest) => 0b0000,
+            Some(Self::AcceleratorPedal) => 0b0001,
+            Some(Self::CruiseControl) => 0b0010,
+            Some(Self::PTOGovernor) => 0b0011,
+            Some(Self::RoadSpeedGovernor) => 0b0100,
+            Some(Self::ASRControl) => 0b0101,
+            Some(Self::TransmissionControl) => 0b0110,
+            Some(Self::ABSControl) => 0b0111,
+            Some(Self::TorqueLimiting) => 0b1000,
+            Some(Self::HighSpeedGovernor) => 0b1001,
+            Some(Self::BrakingSystem) => 0b1010,
+            Some(Self::RemoteAccelerator) => 0b1011,
+            Some(Self::Other) => 0b1111,
+            None => 0b1111,
         }
     }
 }
@@ -140,20 +141,21 @@ impl EngineStarterMode {
         }
     }
 
-    pub fn to_value(mode: Self) -> u8 {
+    pub fn to_value(mode: Option<Self>) -> u8 {
         match mode {
-            Self::StartNotRequested => 0b0000,
-            Self::StarterActiveGearNotEngaged => 0b0001,
-            Self::StarterActiveGearEngaged => 0b0010,
-            Self::StartFinished => 0b0011,
-            Self::StarterInhibitedEngineRunning => 0b0100,
-            Self::StarterInhibitedEngineNotReady => 0b0101,
-            Self::StarterInhibitedTransmissionInhibited => 0b0110,
-            Self::StarterInhibitedActiveImmobilizer => 0b0111,
-            Self::StarterInhibitedOverHeat => 0b1000,
-            Self::Reserved => 0b1001,
-            Self::StarterInhibitedReasonUnknown => 0b1100,
-            Self::Error => 0b1101,
+            Some(Self::StartNotRequested) => 0b0000,
+            Some(Self::StarterActiveGearNotEngaged) => 0b0001,
+            Some(Self::StarterActiveGearEngaged) => 0b0010,
+            Some(Self::StartFinished) => 0b0011,
+            Some(Self::StarterInhibitedEngineRunning) => 0b0100,
+            Some(Self::StarterInhibitedEngineNotReady) => 0b0101,
+            Some(Self::StarterInhibitedTransmissionInhibited) => 0b0110,
+            Some(Self::StarterInhibitedActiveImmobilizer) => 0b0111,
+            Some(Self::StarterInhibitedOverHeat) => 0b1000,
+            Some(Self::Reserved) => 0b1001,
+            Some(Self::StarterInhibitedReasonUnknown) => 0b1100,
+            Some(Self::Error) => 0b1101,
+            None => 0b1111,
         }
     }
 }
@@ -187,15 +189,13 @@ impl ElectronicEngineController1Message {
 
     pub fn to_pdu(&self) -> [u8; 8] {
         [
-            self.engine_torque_mode
-                .map_or(PDU_NOT_AVAILABLE, EngineTorqueMode::to_value),
+            EngineTorqueMode::to_value(self.engine_torque_mode),
             slots::position_level2::enc(self.driver_demand),
             slots::position_level2::enc(self.actual_engine),
             slots::rotational_velocity::enc(self.rpm)[0],
             slots::rotational_velocity::enc(self.rpm)[1],
             slots::source_address::enc(self.source_addr),
-            self.starter_mode
-                .map_or(PDU_NOT_AVAILABLE, EngineStarterMode::to_value),
+            EngineStarterMode::to_value(self.starter_mode),
             PDU_NOT_AVAILABLE,
         ]
     }
@@ -1013,23 +1013,24 @@ impl FanDriveState {
         }
     }
 
-    pub fn to_value(mode: Self) -> u8 {
+    pub fn to_value(mode: Option<Self>) -> u8 {
         match mode {
-            Self::FanOff => 0b0000,
-            Self::EngineSystemGeneral => 0b0001,
-            Self::ExcessiveEngineAirTemperature => 0b0010,
-            Self::ExcessiveEngineOilTemperature => 0b0011,
-            Self::ExcessiveEngineCoolantTemperature => 0b0100,
-            Self::ExcessiveTransmissionOilTemperature => 0b0101,
-            Self::ExcessiveHydraulicOilTemperature => 0b0110,
-            Self::DefaultOperation => 0b0111,
-            Self::NotDefined => 0b1000,
-            Self::ManualControl => 0b1001,
-            Self::TransmissionRetarder => 0b1010,
-            Self::ACSystem => 0b1011,
-            Self::Timer => 0b1100,
-            Self::EngineBrake => 0b1101,
-            Self::Other => 0b1110,
+            Some(Self::FanOff) => 0b0000,
+            Some(Self::EngineSystemGeneral) => 0b0001,
+            Some(Self::ExcessiveEngineAirTemperature) => 0b0010,
+            Some(Self::ExcessiveEngineOilTemperature) => 0b0011,
+            Some(Self::ExcessiveEngineCoolantTemperature) => 0b0100,
+            Some(Self::ExcessiveTransmissionOilTemperature) => 0b0101,
+            Some(Self::ExcessiveHydraulicOilTemperature) => 0b0110,
+            Some(Self::DefaultOperation) => 0b0111,
+            Some(Self::NotDefined) => 0b1000,
+            Some(Self::ManualControl) => 0b1001,
+            Some(Self::TransmissionRetarder) => 0b1010,
+            Some(Self::ACSystem) => 0b1011,
+            Some(Self::Timer) => 0b1100,
+            Some(Self::EngineBrake) => 0b1101,
+            Some(Self::Other) => 0b1110,
+            None => PDU_NOT_AVAILABLE,
         }
     }
 }
@@ -1056,8 +1057,7 @@ impl FanDriveMessage {
     pub fn to_pdu(&self) -> [u8; 8] {
         [
             slots::position_level::enc(self.estimated_percent_fan_speed),
-            self.fan_drive_state
-                .map_or(PDU_NOT_AVAILABLE, FanDriveState::to_value),
+            FanDriveState::to_value(self.fan_drive_state),
             slots::rotational_velocity::enc(self.fan_speed)[0],
             slots::rotational_velocity::enc(self.fan_speed)[1],
             PDU_NOT_AVAILABLE,
@@ -1645,6 +1645,114 @@ impl core::fmt::Display for VehicleElectricalPowerMessage {
     }
 }
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum LampStatus {
+    Off,
+    On,
+    Error,
+}
+
+impl LampStatus {
+    pub fn from_value(value: u8) -> Option<Self> {
+        match value & 0b11 {
+            0b00 => Some(Self::Off),
+            0b01 => Some(Self::On),
+            0b10 => Some(Self::Error),
+            _ => None,
+        }
+    }
+
+    pub fn to_value(mode: Option<Self>) -> u8 {
+        match mode {
+            Some(Self::Off) => 0b00,
+            Some(Self::On) => 0b01,
+            Some(Self::Error) => 0b10,
+            None => 0b11,
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum FlashStatus {
+    Slow,
+    Fast,
+    Reserved,
+}
+
+impl FlashStatus {
+    pub fn from_value(value: u8) -> Option<Self> {
+        match value & 0b11 {
+            0b00 => Some(Self::Slow),
+            0b01 => Some(Self::Fast),
+            0b10 => Some(Self::Reserved),
+            _ => None,
+        }
+    }
+
+    pub fn to_value(mode: Option<Self>) -> u8 {
+        match mode {
+            Some(Self::Slow) => 0b00,
+            Some(Self::Fast) => 0b01,
+            Some(Self::Reserved) => 0b10,
+            None => 0b11,
+        }
+    }
+}
+
+pub struct Diagnostic1Message {
+    pub protect_lamp: Option<LampStatus>,
+    pub amber_warning_lamp: Option<LampStatus>,
+    pub red_stop_lamp: Option<LampStatus>,
+    pub malfunction_indicator_lamp: Option<LampStatus>,
+    pub protect_lamp_flash: Option<FlashStatus>,
+    pub amber_warning_lamp_flash: Option<FlashStatus>,
+    pub red_stop_lamp_flash: Option<FlashStatus>,
+    pub malfunction_indicator_lamp_flash: Option<FlashStatus>,
+    pub suspect_parameter_number: u32,
+    pub failure_mode_identifier: u8,
+    pub spn_conversion_method: u8,
+    pub occurrence_count: u8,
+}
+
+impl Diagnostic1Message {
+    pub fn from_pdu(pdu: &[u8]) -> Self {
+        Self {
+            protect_lamp: LampStatus::from_value(pdu[0]),
+            amber_warning_lamp: LampStatus::from_value(pdu[0] >> 2),
+            red_stop_lamp: LampStatus::from_value(pdu[0] >> 4),
+            malfunction_indicator_lamp: LampStatus::from_value(pdu[0] >> 6),
+            protect_lamp_flash: FlashStatus::from_value(pdu[1]),
+            amber_warning_lamp_flash: FlashStatus::from_value(pdu[1] >> 2),
+            red_stop_lamp_flash: FlashStatus::from_value(pdu[1] >> 4),
+            malfunction_indicator_lamp_flash: FlashStatus::from_value(pdu[1] >> 6),
+            suspect_parameter_number: u32::from_le_bytes([pdu[2], pdu[3], pdu[4] >> 6, 0]),
+            failure_mode_identifier: pdu[4] & 0x1F,
+            spn_conversion_method: pdu[5] >> 7,
+            occurrence_count: pdu[5] & 0x7F,
+        }
+    }
+
+    pub fn to_pdu(&self) -> [u8; 8] {
+        [
+            LampStatus::to_value(self.protect_lamp)
+                | LampStatus::to_value(self.amber_warning_lamp) << 2
+                | LampStatus::to_value(self.red_stop_lamp) << 4
+                | LampStatus::to_value(self.malfunction_indicator_lamp) << 6,
+            FlashStatus::to_value(self.protect_lamp_flash)
+                | FlashStatus::to_value(self.amber_warning_lamp_flash) << 2
+                | FlashStatus::to_value(self.red_stop_lamp_flash) << 4
+                | FlashStatus::to_value(self.malfunction_indicator_lamp_flash) << 6,
+            (self.suspect_parameter_number & 0xFF) as u8,
+            ((self.suspect_parameter_number >> 8) & 0xFF) as u8,
+            ((self.suspect_parameter_number >> 16) & 0x3F) as u8
+                | (self.failure_mode_identifier & 0x1F),
+            (self.spn_conversion_method & 0x01) << 7 | (self.occurrence_count & 0x7F),
+            PDU_NOT_AVAILABLE,
+            PDU_NOT_AVAILABLE,
+        ]
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -2012,6 +2120,148 @@ mod tests {
         assert_eq!(
             electrical_power_message_decoded.battery_potential,
             Some(947)
+        );
+    }
+
+    #[test]
+    fn diagnostic_1_message_1() {
+        let diagnostic_message =
+            Diagnostic1Message::from_pdu(&[0x57, 0xFF, 0x9F, 0x00, 0x03, 0x01]);
+
+        assert_eq!(diagnostic_message.protect_lamp, None);
+        assert_eq!(diagnostic_message.amber_warning_lamp, Some(LampStatus::On));
+        assert_eq!(diagnostic_message.red_stop_lamp, Some(LampStatus::On));
+        assert_eq!(
+            diagnostic_message.malfunction_indicator_lamp,
+            Some(LampStatus::On)
+        );
+        assert_eq!(diagnostic_message.protect_lamp_flash, None);
+        assert_eq!(diagnostic_message.amber_warning_lamp_flash, None);
+        assert_eq!(diagnostic_message.red_stop_lamp_flash, None);
+        assert_eq!(diagnostic_message.malfunction_indicator_lamp_flash, None);
+        assert_eq!(diagnostic_message.suspect_parameter_number, 159);
+        assert_eq!(diagnostic_message.failure_mode_identifier, 3);
+        assert_eq!(diagnostic_message.spn_conversion_method, 0);
+        assert_eq!(diagnostic_message.occurrence_count, 1);
+    }
+
+    #[test]
+    fn diagnostic_1_message_2() {
+        let diagnostic_message =
+            Diagnostic1Message::from_pdu(&[0x57, 0xFF, 0xFB, 0x06, 0x0B, 0x32]);
+
+        assert_eq!(diagnostic_message.protect_lamp, None);
+        assert_eq!(diagnostic_message.amber_warning_lamp, Some(LampStatus::On));
+        assert_eq!(diagnostic_message.red_stop_lamp, Some(LampStatus::On));
+        assert_eq!(
+            diagnostic_message.malfunction_indicator_lamp,
+            Some(LampStatus::On)
+        );
+        assert_eq!(diagnostic_message.protect_lamp_flash, None);
+        assert_eq!(diagnostic_message.amber_warning_lamp_flash, None);
+        assert_eq!(diagnostic_message.red_stop_lamp_flash, None);
+        assert_eq!(diagnostic_message.malfunction_indicator_lamp_flash, None);
+        assert_eq!(diagnostic_message.suspect_parameter_number, 1787);
+        assert_eq!(diagnostic_message.failure_mode_identifier, 11);
+        assert_eq!(diagnostic_message.spn_conversion_method, 0);
+        assert_eq!(diagnostic_message.occurrence_count, 50);
+    }
+
+    #[test]
+    fn diagnostic_1_message_3() {
+        let diagnostic_message =
+            Diagnostic1Message::from_pdu(&[0x40, 0xFF, 0x7F, 0x02, 0x02, 0x00]);
+
+        assert_eq!(diagnostic_message.protect_lamp, Some(LampStatus::Off));
+        assert_eq!(diagnostic_message.amber_warning_lamp, Some(LampStatus::Off));
+        assert_eq!(diagnostic_message.red_stop_lamp, Some(LampStatus::Off));
+        assert_eq!(
+            diagnostic_message.malfunction_indicator_lamp,
+            Some(LampStatus::On)
+        );
+        assert_eq!(diagnostic_message.protect_lamp_flash, None);
+        assert_eq!(diagnostic_message.amber_warning_lamp_flash, None);
+        assert_eq!(diagnostic_message.red_stop_lamp_flash, None);
+        assert_eq!(diagnostic_message.malfunction_indicator_lamp_flash, None);
+        assert_eq!(diagnostic_message.suspect_parameter_number, 639);
+        assert_eq!(diagnostic_message.failure_mode_identifier, 2);
+        assert_eq!(diagnostic_message.spn_conversion_method, 0);
+        assert_eq!(diagnostic_message.occurrence_count, 0);
+    }
+
+    #[test]
+    fn diagnostic_1_message_4() {
+        let diagnostic_message_encoded = Diagnostic1Message {
+            protect_lamp: Some(LampStatus::Off),
+            amber_warning_lamp: Some(LampStatus::Off),
+            red_stop_lamp: Some(LampStatus::Off),
+            malfunction_indicator_lamp: Some(LampStatus::On),
+            protect_lamp_flash: None,
+            amber_warning_lamp_flash: None,
+            red_stop_lamp_flash: None,
+            malfunction_indicator_lamp_flash: None,
+            suspect_parameter_number: 639,
+            failure_mode_identifier: 2,
+            spn_conversion_method: 0,
+            occurrence_count: 0,
+        }
+        .to_pdu();
+
+        assert_eq!(
+            diagnostic_message_encoded,
+            [0x40, 0xFF, 0x7F, 0x02, 0x02, 0x00, 0xFF, 0xFF]
+        );
+    }
+
+    #[test]
+    fn diagnostic_1_message_5() {
+        let diagnostic_message_encoded = Diagnostic1Message {
+            protect_lamp: Some(LampStatus::On),
+            amber_warning_lamp: Some(LampStatus::Off),
+            red_stop_lamp: Some(LampStatus::On),
+            malfunction_indicator_lamp: Some(LampStatus::On),
+            protect_lamp_flash: Some(FlashStatus::Slow),
+            amber_warning_lamp_flash: None,
+            red_stop_lamp_flash: Some(FlashStatus::Slow),
+            malfunction_indicator_lamp_flash: Some(FlashStatus::Fast),
+            suspect_parameter_number: 639,
+            failure_mode_identifier: 2,
+            spn_conversion_method: 0,
+            occurrence_count: 0,
+        }
+        .to_pdu();
+
+        let diagnostic_message_decoded = Diagnostic1Message::from_pdu(&diagnostic_message_encoded);
+
+        assert_eq!(
+            diagnostic_message_decoded.protect_lamp,
+            Some(LampStatus::On)
+        );
+        assert_eq!(
+            diagnostic_message_decoded.amber_warning_lamp,
+            Some(LampStatus::Off)
+        );
+        assert_eq!(
+            diagnostic_message_decoded.red_stop_lamp,
+            Some(LampStatus::On)
+        );
+        assert_eq!(
+            diagnostic_message_decoded.malfunction_indicator_lamp,
+            Some(LampStatus::On)
+        );
+
+        assert_eq!(
+            diagnostic_message_decoded.protect_lamp_flash,
+            Some(FlashStatus::Slow)
+        );
+        assert_eq!(diagnostic_message_decoded.amber_warning_lamp_flash, None);
+        assert_eq!(
+            diagnostic_message_decoded.red_stop_lamp_flash,
+            Some(FlashStatus::Slow)
+        );
+        assert_eq!(
+            diagnostic_message_decoded.malfunction_indicator_lamp_flash,
+            Some(FlashStatus::Fast)
         );
     }
 }

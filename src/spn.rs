adb9654dd4b70764a669d156327b24c86fe82fec
Yorick de Wid <yorickdewid@users.noreply.github.com>
2024-03-12 18:15:50 +0100

    Add tests for ElectronicEngineController1Message

--- src/spn.rs
+++ src/spn.rs
@@ -1702,6 +1702,19 @@ mod tests {
 
     #[test]
     fn electronic_engine_controller_1_message_4() {
+        let engine_message = ElectronicEngineController1Message::from_pdu(&[
+            0xFF, 0x00, 0x7D, 0x00, 0x00, 0x32, 0xFF, 0xFF,
+        ]);
+        assert_eq!(engine_message.engine_torque_mode, None);
+        assert_eq!(engine_message.driver_demand, Some(0));
+        assert_eq!(engine_message.actual_engine, Some(0));
+        assert_eq!(engine_message.rpm, Some(0));
+        assert_eq!(engine_message.source_addr, Some(0x32));
+        assert_eq!(engine_message.starter_mode, None);
+    }
+
+    #[test]
+    fn electronic_engine_controller_1_message_5() {
         let engine_message_encoded = ElectronicEngineController1Message {
             engine_torque_mode: Some(EngineTorqueMode::HighSpeedGovernor),
             driver_demand: Some(93),

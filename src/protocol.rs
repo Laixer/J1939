53c9eac7bca9225b9c20556e61bb7686a0159a24
Yorick de Wid <yorickdewid@users.noreply.github.com>
2024-06-10 15:07:54 +0200

    add commanded_address function

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,4 +1,6 @@
-use crate::{Frame, FrameBuilder, IdBuilder, Name, PDU_NOT_AVAILABLE, PGN};
+use crate::{
+    transport::BroadcastTransport, Frame, FrameBuilder, IdBuilder, Name, PDU_NOT_AVAILABLE, PGN,
+};
 
 /// Create PGN request frame.
 pub fn request(da: u8, sa: u8, pgn: PGN) -> Frame {
@@ -49,4 +51,29 @@ pub fn acknowledgement(sa: u8, pgn: PGN) -> Frame {
             pgn_bytes[2],
         ])
         .build()
+}
+
+/// Assign address to node.
+pub fn commanded_address(sa: u8, name: &Name, address: u8) -> [Frame; 3] {
+    let name_bytes = name.to_bytes();
+
+    let data = [
+        name_bytes[0],
+        name_bytes[1],
+        name_bytes[2],
+        name_bytes[3],
+        name_bytes[4],
+        name_bytes[5],
+        name_bytes[6],
+        name_bytes[7],
+        address,
+    ];
+
+    let mut transport = BroadcastTransport::new(sa, PGN::CommandedAddress).with_data(&data);
+
+    let frame0 = transport.next_frame();
+    let frame1 = transport.next_frame();
+    let frame2 = transport.next_frame();
+
+    [frame0, frame1, frame2]
 }
